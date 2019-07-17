e3d3096c06f67d8988e0891c7b43427dca5a7acf
wangh <wangh_815@163.com>
2019-07-17 23:59:39 +0800

    update

--- 11-20/11-10000之内的素数写入文件.py
+++ 11-20/11-10000之内的素数写入文件.py
@@ -1,24 +1,24 @@
 from math import sqrt
 
 
-def is_prim(n):
-    assert n > 0
-    for factor in range(2, int(sqrt(n)) + 1):
-        if n % factor == 0:
+def is_prim(n):    # 判断是否是素数
+    assert n > 0   # 断言，要求n必须大于0
+    for factor in range(2, int(sqrt(n)) + 1):  # 缩小取数范围
+        if n % factor == 0:                    # 取是否能整除
             return False
-    return True if n != 1 else False
+    return True if n != 1 else False           # 去掉1
 
 
 def main():
-    files = ('a.txt', 'b.txt', 'c.txt')
-    fs_list = []
+    files = ('a.txt', 'b.txt', 'c.txt')   # 文件名元组(tuple)，有序不可变
+    fs_list = []                          # 空列表(list)，有序可变
     try:
         for file in files:
-            fs_list.append(open(file, 'w', encoding='utf-8'))
+            fs_list.append(open(file, 'w', encoding='utf-8'))  # 创建文件
         for num in range(1, 10000):
             if is_prim(num):
                 if num < 100:
-                    fs_list[0].write(str(num) + '\n')
+                    fs_list[0].write(str(num) + '\n')  # 写入文件
                 elif num < 1000:
                     fs_list[1].write(str(num) + '\n')
                 else:
